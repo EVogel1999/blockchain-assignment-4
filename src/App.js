2510870e0c8abc8b52eaa6c847ebbc97e9153a53
Emily Vogel <eav990@gmail.com>
2022-05-02 15:48:26 -0400

    Finish assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+// IPFS dependencies
+import { create } from 'ipfs-http-client';
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
 function App() {
+
+  // State
+  const [file, setFile] = useState('');
+  const [notesList, setNotesList] = useState([]);
+
+  /**
+   * Uploads a file to IPFS
+   */
+  const uploadFile = async () => {
+    const { cid } = await client.add(file);
+    
+    if (notesList.find(note => note.cid === cid.toString())) {
+      alert('This note has already been uploaded');
+    } else {
+      setNotesList([...notesList, { cid: cid.toString(), name: file.name }]);
+    }
+  };
+
+  /**
+   * Changes the file to upload next
+   * @param {*} event the event for changing the file to upload
+   */
+  const changeFile = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  // Construct the list of uploaded notes
+  const list = (
+    <ul>
+      {(notesList || []).map(note => {
+          return <li key={note.cid}>{note.name} - <a href={`https://ipfs.io/ipfs/${note.cid}`} target="_blank">{note.cid}</a></li>
+        })}
+    </ul>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>File Upload</h1>
+        <input type="file" id="file" name="file" onChange={changeFile}></input>
+        <button onClick={uploadFile}>Upload</button>
+      </div>
+      <div>
+        <h1>Your Uploaded Notes</h1>
+        {(notesList && notesList.length > 0) && list}
+        {!(notesList && notesList.length > 0) && <p>No notes have been uploaded yet</p>}
+      </div>
     </div>
   );
 }
